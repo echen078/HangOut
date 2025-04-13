7ba3202561490a4968776ea120a181cd33cf5492
ethanlgit <ethanlimh@yahoo.com>
2025-04-13 01:29:15 -0700

    update

--- card.js
+++ card.js
@@ -1,8 +1,20 @@
 window.onload = function() {
-    card = document.getElementById("card");
+
+
+    outer_card = document.getElementById("outer-card");
+    inner_card = document.getElementById("inner-card");
     
-    card.addEventListener("click", () => {
-        console.log("hello");
+
+    document.addEventListener("keydown", (e) => {
+        
+        if (e.key == 'ArrowLeft') {
+            outer_card.style.animation = "slideLeft 1s ease-in-out forwards";
+            inner_card.style.animation = "rotateLeft 1s ease-in         forwards, fadeOut 1s ease-in forwards"; 
+        } else if (e.key == 'ArrowRight') {
+            outer_card.style.animation = "slideRight 1s ease-in-out forwards";
+            inner_card.style.animation = "rotateRight 1s ease-in         forwards, fadeOut 1s ease-in forwards";
+        }
+        
     });
     
 }
